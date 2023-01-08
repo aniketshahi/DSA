1cec611ff511d5ebf716dae5796e218b43e6ca6b
aniket0608 <aniketshahi7766@gmail.com>
2023-01-08 12:38:28 +0530

    modified:   pratice2.cpp 	modified:   pratice2.exe

--- pratice2.cpp
+++ pratice2.cpp
@@ -211,3 +211,47 @@
 
 //    return 0;
 // }
+
+// linked list
+
+// #include <iostream>
+// using namespace std;
+// class node
+// {
+// public:
+//     int data;
+//     node *next;
+//     node(int data)
+//     {
+//         this->data = data;
+//         this->next = NULL;
+//     }
+// };
+// void insertAthead(node *&head, int d)
+// {
+//     node *temp = new node(d);
+//     temp->next = head;
+//     head = temp;
+// }
+// void printNode(node *&head)
+// {
+//     node *temp = head;
+//     while (temp != NULL)
+//     {
+//         cout << temp->data << " ";
+//         temp = temp->next;
+//     }
+//     cout << endl;
+// }
+// int main()
+// {
+//     node *node1 = new node(10);
+//     // cout << node1->data << endl;
+//     // cout << node1->next << endl;
+//     node *head = node1;
+//     printNode(head);
+//     insertAthead(head, 12);
+//     printNode(head);
+
+//     return 0;
+// }
