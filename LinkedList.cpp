c5a8eaef5206a5bbacf7fd3e83669c84b84212fd
aniket0608 <aniketshahi7766@gmail.com>
2023-01-16 21:48:36 +0530

    modified:   LinkedList.cpp

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -16,8 +16,43 @@ class node{
         next =NULL;
     }
 };
+class singlyLinkedList{
+   public:
+   node *head;
+   singlyLinkedList(node* h){
+    head=NULL;
+   }
+   singlyLinkedList(NODE* n){
+       head=n;
+   }
+   node*nodeExits(int k){
+    node*temp=NULL;
+    node*ptr=head;
+    while(pyr!=NULL){
+        if(ptr->key==k){
+            temp=ptr;
+        }
+        ptr=ptr->nextl;
+    }
+   }
+   void prependnode(node* h){
+      
+   }
+   void appendnode(node* h);
+   void insertnode(key);
+   void deletenote(key);
+   void updatenode(key);
+
+};
 int main()
 {
+    node n1(1,10);
+    node n2(2,10);
+    node n3(3,30);
+    singlyLinkedList s(&n1);
+    s.appendnode(&n2);
+    s.prependnode(&n3);
+
 
 return 0;
 }
