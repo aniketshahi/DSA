fdc95ac7d27b663446d075e0b6389c03e60267e3
aniket0608 <aniketshahi7766@gmail.com>
2023-01-17 08:50:33 +0530

    new file:   Linkedlist.exe

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,58 +1,52 @@
 #include <iostream>
 using namespace std;
-class node{
-    public:
+class node
+{
+public:
     int key;
     int data;
-    node* next;
-    node(){
-        key =0;
-        data=0;
-        next =NULL;
-    } 
-    node(int k,int d){
-        key=k;
-        data =d;
-        next =NULL;
+    node *next;
+    node()
+    {
+        key = 0;
+        data = 0;
+        next = NULL;
+    }
+    node(int k, int d)
+    {
+        key = k;
+        data = d;
+        next = NULL;
     }
 };
-class singlyLinkedList{
-   public:
-   node *head;
-   singlyLinkedList(node* h){
-    head=NULL;
-   }
-   singlyLinkedList(NODE* n){
-       head=n;
-   }
-   node*nodeExits(int k){
-    node*temp=NULL;
-    node*ptr=head;
-    while(pyr!=NULL){
-        if(ptr->key==k){
-            temp=ptr;
-        }
-        ptr=ptr->nextl;
+void insertAthead(node *&head, int d)
+{
+    node temp = new node(d);
+    temp->next = head;
+    head = temp;
+}
+void print(node *&head)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data;
+        temp = temp->next;
     }
-   }
-   void prependnode(node* h){
-      
-   }
-   void appendnode(node* h);
-   void insertnode(key);
-   void deletenote(key);
-   void updatenode(key);
-
-};
+    cout << endl;
+}
 int main()
 {
-    node n1(1,10);
-    node n2(2,10);
-    node n3(3,30);
-    singlyLinkedList s(&n1);
-    s.appendnode(&n2);
-    s.prependnode(&n3);
+    node n1(1, 10);
+    node n2(2, 10);
+    node n3(3, 30);
+    // singlyLinkedList s(&n1);
+    // s.appendnode(&n2);
+    // s.prependnode(&n3);
+    node *node1 = new node(4, 40);
+    cout << node1->key << endl;
+    cout << node1->data << endl;
+    cout << node1->next << endl;
 
-
-return 0;
+    return 0;
 }
---
+++ Prefix.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stack>;
+using namespace std;
+string infixTopostfix(stack<char> s, string infix)
+{
+    string postfix;
+for(int i=0;i<infix.length();i++){
+
+ 
+
+
+
+}
+
+    return postfix;
+}
+int main()
+{
+string infix_exp, postfix_exp;
+cout << "Enter an inifix expression" << endl;
+cin >> infix_exp;
+stack<char> stack;
+cout << "Infix Expression: " << infix_exp << endl;
+postfix_exp = infixTopostfix(stack, infix_exp);
+cout << "Postfix Expresion: " << postfix_exp;
+return 0;
+}
