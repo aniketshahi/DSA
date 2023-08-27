4f790d9754fbba1d94e253e4260c0fc86708b08a
aniket0608 <aniketshahi7766@gmail.com>
2023-08-27 15:11:00 +0530

    modified:   loop.cpp

--- loop.cpp
+++ loop.cpp
@@ -1,11 +1,13 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-  int i = 0;
-  for (i = 0; i < 10; i++) // iteration
-  {
-    cout << i << endl;
-  }
-  return 0;
-}
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     while (true) 
+//     {
+       
+//         cout << "5" << endl;
+//     }
+
+//     return 0;
+// }
