2113d4fb9e696057005ee9153a3cdac213fc6610
aniket0608 <aniketshahi7766@gmail.com>
2023-10-01 01:05:21 +0530

    modified:   BubbleSort.cpp 	modified:   InsertionSort.cpp

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -1,41 +1,40 @@
-// #include <iostream>
-// using namespace std;
+#include <iostream>
+using namespace std;
 
-// int BubleSort(int arr[], int n)
-// {
-//     int temp;
-//     for (int i = 0; i < n - 1; i++)
-//     {
-//         for (int j = 0; j < n - 1 - i; j++)
-//         {
-//             if (arr[j] > arr[j + 1])
-//             {
-//                 temp = arr[j];
-//                 arr[j] = arr[j + 1];
-//                 arr[j + 1] = temp;
-//             }
-//         }
-//     }
-// }
-// void printArray(int arr[], int size)
-// {
+void BubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        bool swapped = false;  // Optimization: Track if any swaps were made in this pass
+        for (int j = 0; j < n - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;  // Set swapped to true if a swap occurred
+            }
+        }
+        if (!swapped) {
+            // If no swaps were made in this pass, the array is already sorted
+            break;
+        }
+    }
+}
 
-//     for (int i = 0; i < size; i++)
-//         cout << arr[i] << " ";
-//     cout << endl;
-// }
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << arr[i];
+        if (i < size - 1) {
+            cout << " ";
+        }
+    }
+    cout << endl;
+}
 
-// int main()
-// {
-//     int arr[] = {8, 4, 11, 32, 1};
-//     int n = sizeof(arr) / sizeof(arr[0]);
-//     cout << "Unsorted Array" << endl;
-//     printArray(arr, n);
-//     cout << endl
-//          << endl;
-//     BubleSort(arr, n);
-//     cout << "Sorted array" << endl;
-//     printArray(arr, n);
-//     return 0;
-//     return 0;
-// }
+int main() {
+    int arr[] = {8, 4, 11, 32, 1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << "Unsorted Array" << endl;
+    printArray(arr, n);
+    cout << endl;
+    BubbleSort(arr, n);
+    cout << "Sorted array" << endl;
+    printArray(arr, n);
+    return 0;
+}
--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -1,39 +1,38 @@
 #include <iostream>
 using namespace std;
-void InsertionSort(int arr[], int n)
-{
-    int key, j;
-    for (int i = 0; i < n; i++)
-    {
-        key = arr[i];
-        j = i - 1;
-        while (j >= 0 && arr[j] > key)
-        {
+
+void InsertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        
+        while (j >= 0 && arr[j] > key) {
             arr[j + 1] = arr[j];
             j--;
         }
+        
         arr[j + 1] = key;
     }
 }
-void printArray(int arr[], int size)
-{
 
-    for (int i = 0; i < size; i++)
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
         cout << arr[i] << " ";
+    }
     cout << endl;
 }
 
-int main()
-{
+int main() {
     int arr[] = {8, 4, 11, 32, 1};
     int n = sizeof(arr) / sizeof(arr[0]);
+    
     cout << "Unsorted Array" << endl;
     printArray(arr, n);
-    cout << endl
-         << endl;
+    
     InsertionSort(arr, n);
+    
     cout << "Sorted array" << endl;
     printArray(arr, n);
-    return 0;
+    
     return 0;
 }
