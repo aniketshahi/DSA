66bb1d13582c1029deb98104941acdeaffe15c9f
aniket0608 <aniketshahi7766@gmail.com>
2023-10-01 01:06:30 +0530

    modified:   MergeSort.cpp

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -1,74 +1,64 @@
-// #include <iostream>
-// using namespace std;
-// // declaring merge function
-// void Merge(int arr[], int low, int mid, int high)
-// {
-//     int i, j, k, b[100];
-//     i = low;
-//     j = mid + 1;
-//     k = 0;
+#include <iostream>
 
-//     while (i <= mid && j <= high)
-//     {
-//         if (arr[i] < arr[j])
-//         {
-//             b[k] = arr[i];
-//             i++, k++;
-//         }
-//         else
-//             b[k] = arr[j];
-//         j++, k++;
-//     }
-//     while (i <= mid)
-//     {
-//         b[k] = arr[i];
-//         k++, i++;
-//     }
-//     while (j <= high)
-//     {
-//         b[k] = arr[j];
-//         k++, j++;
-//     }
-//     for (int i = low; i <= high; i++)
-//     {
-//         arr[i] = b[i];
-//     }
-// }
-// // Function for sorting array
-// void MergeSort(int arr[], int low, int high)
-// {
-//     int mid;
-//     if (low < high)
-//     {
-//         mid = (low + high) / 2;
-//         MergeSort(arr, low, mid);
-//         MergeSort(arr, mid + 1, high);
-//         Merge(arr, low, mid, high);
-//     }
-// }
-// // function for printing array
-// void printArray(int arr[], int size)
-// {
+// Declaring merge function
+void Merge(int arr[], int low, int mid, int high) {
+    int i = low, j = mid + 1, k = 0;
+    int* b = new int[high - low + 1];
 
-//     for (int i = 0; i < size; i++)
-//         cout << arr[i] << " ";
-//     cout << endl;
-// }
-// // main function
-// int main()
-// {
-//     int arr[] = {8, 4, 11, 32, 1};
-//     int low = arr[0];
+    while (i <= mid && j <= high) {
+        if (arr[i] < arr[j]) {
+            b[k++] = arr[i++];
+        } else {
+            b[k++] = arr[j++];
+        }
+    }
 
-//     int n = sizeof(arr) / sizeof(arr[0]);
-//     int high = arr[n - 1];
-//     cout << "Unsorted Array" << endl;
-//     printArray(arr, n);
-//     cout << endl
-//          << endl;
-//     MergeSort(arr, low, high);
-//     cout << "Sorted array" << endl;
-//     printArray(arr, n);
-//     return 0;
-//     return 0;
-// }
+    while (i <= mid) {
+        b[k++] = arr[i++];
+    }
+
+    while (j <= high) {
+        b[k++] = arr[j++];
+    }
+
+    for (i = 0; i < k; i++) {
+        arr[low + i] = b[i];
+    }
+
+    delete[] b;
+}
+
+// Function for sorting array
+void MergeSort(int arr[], int low, int high) {
+    if (low < high) {
+        int mid = low + (high - low) / 2;
+        MergeSort(arr, low, mid);
+        MergeSort(arr, mid + 1, high);
+        Merge(arr, low, mid, high);
+    }
+}
+
+// Function for printing array
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+// Main function
+int main() {
+    int arr[] = {8, 4, 11, 32, 1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    std::cout << "Unsorted Array" << std::endl;
+    printArray(arr, n);
+    std::cout << std::endl;
+
+    MergeSort(arr, 0, n - 1);
+
+    std::cout << "Sorted array" << std::endl;
+    printArray(arr, n);
+
+    return 0;
+}
